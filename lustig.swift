e914ae1a395e0ddabfd0cd477cc41a6b60748462
The one who collects what the other dumps is an Ironman. <60981745+pankourgiou@users.noreply.github.com>
2026-06-27 10:07:10 +0300

    Create lustig.swift

---
+++ lustig.swift
@@ -0,0 +1,12 @@
+//Deutsch lang ist constructivistic lang...latin basis. Das ist ein programme auf swift
+print("Griechenland ist Schön, Anna ist Schön. Strom ist Macht-->energie ist macht. European mensch habst macht. Anna ist ein gut person.")
+//not without problems
+print("mein freund ist ein gut person")
+//not without problems
+print("Ich liebe sommer und die Lustig haben. Ich liebe die Buch lesen. Ich liebe die Buch schriben und Ich liebe hören ein Hörbuch. Ich liebe fussball spielen und ich spiel li(e)bero position:)Michael Haneke-Amour ist ein excellent/elegant Film. Hermann Esse der Steppenwolf ist ein Buch Alexndra geben auf mich und ist gut")
+//Mein Vater Leonidas habst auf Mercdes Benz arbeit
+//Meine Schwester sprachts die Deutsch
+//Mein freund Peter sprachst auf mich für Escher maler und Escher ist gut.
+print("Wie gehts Anna? Bist du gut? Alles klar? Alles in ordung? Weiss du WW2 geschichte? Weiss du Enigma maschine und Alan Turing? er is ein Anmut/Charme auf Frieden und Krieg, auf weiss und Schwarz...Ich möchte/traume der mench lieb die Hörbuch hören und denken. Denken poecia, philosophie und in Freiheit leben")
+//Ist Freiheit und democracie ein problem? 
+//Kannst du ohne Nachdenken und freies Denken leben?
